3f522ce0a09b58be981b194e6009019b49aa7398
Rohan Varma <rvarm1@gmail.com>
2017-12-17 21:30:11 -0800

    added job adding

--- generator.py
+++ generator.py
@@ -1,4 +1,7 @@
 from collections import deque
+import threading
+from threading import Thread
+from time import sleep
 
 def my_xrange(i, job_id):
 	cur = 0
@@ -20,12 +23,23 @@ class Scheduler(object):
 			finished = False
 			# run the task until it yields
 			try:
+				sleep(1)
 				next(current_task)
 			except StopIteration:
 				# if we're here that means the task is done, so don't add it back to the list
 				finished = True
 			if not finished:
 				self.waiting_tasks.append(current_task)
+
+	def add_jobs(self, num_jobs):
+		print('adding {} jobs'.format(num_jobs))
+		new_jobs = [my_xrange(50, 6 + i) for i in range(num_jobs)]
+		for job in new_jobs:
+			print('adding a job')
+			self.waiting_tasks.append(job)
+			sleep(1)
+
+
 
 
 
@@ -41,6 +55,10 @@ if __name__ == '__main__':
 
 	tasks = [my_xrange(5, i) for i in range(5)]
 	scheduler = Scheduler(tasks)
-	scheduler.run_scheduled_jobs()
-
-	
+	task_runner = Thread(target = scheduler.run_scheduled_jobs, name = 'task-runner')
+	job_adder = Thread(target = scheduler.add_jobs, name = 'job-adder', kwargs ={'num_jobs': 10})
+	threads = [task_runner, job_adder]
+	for thread in threads:
+		thread.start()
+	for thread in threads:
+		thread.join()
